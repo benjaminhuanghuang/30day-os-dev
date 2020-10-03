006eb8b45c3c9ff1f7fbf50ec831144a10fb042e
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-10-03 11:24:15 -0700

    can load loader!

--- d2-boot-loader-2/run.sh
+++ d2-boot-loader-2/run.sh
@@ -1,10 +1,14 @@
 rm myos.img
 
 # Create a img file
-dd if=loader.bin of=myos.img count=1 bs=512 conv=notrunc
-sudo mount -o loop -t vfat myos.img /media/floppy1
+dd if=/dev/zero of=myos.img bs=512 count=2880
+mkfs.vfat -F 12 myos.img
+
+sudo mount -o loop myos.img /media/floppy1
 sudo cp loader.bin /media/floppy1/ -v
 sudo umount /media/floppy1
 
-# qemu-system-x86_64 -fda myos.img -boot a
+dd if=boot.bin of=myos.img count=1 bs=512 conv=notrunc
+
+qemu-system-x86_64 -fda myos.img -boot a
 
