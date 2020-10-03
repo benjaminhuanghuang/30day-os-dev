cc5ee900b3b3cb6d6cb489b5bedec0d98bd167b4
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-10-03 10:45:04 -0700

    fix run.sh

--- d2-boot-loader-2/run.sh
+++ d2-boot-loader-2/run.sh
@@ -2,8 +2,8 @@ rm myos.img
 
 # Create a img file
 dd if=loader.bin of=myos.img count=1 bs=512 conv=notrunc
-sudo mount -o loop myos.img /media/floppy1
-sudo cp kernel.bin /media/floppy1/ -v
+sudo mount -o loop -t vfat myos.img /media/floppy1
+sudo cp loader.bin /media/floppy1/ -v
 sudo umount /media/floppy1
 
 # qemu-system-x86_64 -fda myos.img -boot a
