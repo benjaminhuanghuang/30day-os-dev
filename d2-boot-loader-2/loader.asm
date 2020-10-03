7a252893010362a06d905fda12016d1fc3edb4c6
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-10-03 11:29:32 -0700

    update

--- d2-boot-loader-2/loader.asm
+++ d2-boot-loader-2/loader.asm
@@ -6,6 +6,10 @@
   MOV		AL,0x13			; VGA, 320x200x8bit color
   MOV		AH,0x00
   INT		0x10
+
+  MOV    AH,'L'      ; 显示一个文字
+  MOV    BX,15        ; 指定字符颜色
+  INT    0x10         ; 调用显卡BIOS
 fin:
   HLT
   JMP		fin
