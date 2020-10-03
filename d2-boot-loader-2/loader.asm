6834b5dfe8c36108a88e27a904f722298371663e
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-10-03 11:59:18 -0700

    update

--- d2-boot-loader-2/loader.asm
+++ d2-boot-loader-2/loader.asm
@@ -1,15 +1,30 @@
 ; kernel.bin is at 0x4200 on disk, 
 ; The disk will be load to 0x8000 in memory, So the address is 0x8000+0x4200 = 0xc200
 
-  ORG		0xc200			;
+; BOOT_INFO関係
+CYLS	EQU		0x0ff0			; ブートセクタが設定する
+LEDS	EQU		0x0ff1
+VMODE	EQU		0x0ff2			; how many bits for color？
+SCRNX	EQU		0x0ff4			; 分辨率X
+SCRNY	EQU		0x0ff6			; 分辨率Y
+VRAM	EQU		0x0ff8			; 图形缓冲区起始地址
 
-  MOV		AL,0x13			; VGA, 320x200x8bit color
-  MOV		AH,0x00
-  INT		0x10
 
-  MOV    AH,'L'      ; 显示一个文字
-  MOV    BX,15        ; 指定字符颜色
-  INT    0x10         ; 调用显卡BIOS
+		ORG		0xc200			; 
+
+		MOV		AL,0x13			; VGA图形，320x200x8bit彩色
+		MOV		AH,0x00
+		INT		0x10
+		MOV		BYTE [VMODE],8 	       ;  record mode
+		MOV		WORD [SCRNX],320
+		MOV		WORD [SCRNY],200
+		MOV		DWORD [VRAM],0x000a0000
+
+; Get keyboard LED status
+		MOV		AH,0x02
+		INT		0x16 			; keyboard BIOS
+		MOV		[LEDS],AL
+
 fin:
-  HLT
-  JMP		fin
+		HLT
+		JMP		fin
