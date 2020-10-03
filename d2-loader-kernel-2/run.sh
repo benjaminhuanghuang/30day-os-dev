bc2e9f0b784a5271b8f55556d09ae2383564aef9
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-10-03 08:31:53 -0700

    updat

--- d2-loader-kernel-2/run.sh
+++ d2-loader-kernel-2/run.sh
@@ -1,7 +1,8 @@
 rm myos.img
 
 # Create a img file
-qemu-img create -f raw myos.img 1440k
+#qemu-img create -f raw myos.img 1440k
+dd if=/dev/zero of=myos.img bs=512 count=2880
   
 # Wirte loader.bin (add 0x55AA) and kernel.bin to myos.img by using the java app
 dd if=loader.bin of=myos.img count=1 bs=512 conv=nottunc
