8cdc90656372ea593fec6987ccbf9a5f8a00aa27
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-10-03 07:38:34 -0700

    udate

--- d2-loader-kernel-2/run.sh
+++ d2-loader-kernel-2/run.sh
@@ -4,8 +4,9 @@ rm myos.img
 qemu-img create -f raw myos.img 1440k
   
 # Wirte loader.bin (add 0x55AA) and kernel.bin to myos.img by using the java app
-dd if=loader.bin of=myos.img count=1 bs=512
-dd if=kernal.bin of=myos.img
+dd if=loader.bin of=myos.img count=1 bs=512 conv=nottunc
+sudo mount -o loop - t vfat ./myos.img /media/floppy1
+cp kernel.bin /media/floppy1/kenel.bin
 
 qemu-system-x86_64 -fda myos.img -boot a
 
