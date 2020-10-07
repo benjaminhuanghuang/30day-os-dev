29ec8d5985878c4c7b117efd8c3e5f1483bb45f9
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-10-07 08:41:54 -0700

    draw mouse cursor

--- 05-text-mouse/bootpack.c
+++ 05-text-mouse/bootpack.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h>   // in the C compiler by the author
 
 extern void io_hlt(void);
 extern void io_cli(void);
@@ -15,7 +15,10 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 void init_screen(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
-
+// Mouse
+void init_mouse_cursor8(char *mouse, char bc);
+void putblock8_8(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize);
 
 /*
 	color name in the palette
@@ -74,6 +77,12 @@ void HariMain(void)
 	sprintf(s, "scrnx = %d", binfo->scrnx);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
 
+	// Mouse
+	char mcursor[256];
+	init_mouse_cursor8(mcursor, COL8_008484);
+	int mx = (binfo->scrnx - 16)/2;
+	int my = (binfo->scrny - 28 - 16)/2;
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 	for (;;)
 	{
 		io_hlt();
@@ -186,6 +195,58 @@ void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s
 	for (; *s != 0x00; s++) {
 		putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
 		x += 8;
+	}
+	return;
+}
+
+
+void init_mouse_cursor8(char *mouse, char bc)
+/* cursor（16x16） */
+{
+	static char cursor[16][16] = {
+		"**************..",
+		"*OOOOOOOOOOO*...",
+		"*OOOOOOOOOO*....",
+		"*OOOOOOOOO*.....",
+		"*OOOOOOOO*......",
+		"*OOOOOOO*.......",
+		"*OOOOOOO*.......",
+		"*OOOOOOOO*......",
+		"*OOOO**OOO*.....",
+		"*OOO*..*OOO*....",
+		"*OO*....*OOO*...",
+		"*O*......*OOO*..",
+		"**........*OOO*.",
+		"*..........*OOO*",
+		"............*OO*",
+		".............***"
+	};
+	int x, y;
+
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 16; x++) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 16 + x] = COL8_000000;
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 16 + x] = COL8_FFFFFF;
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 16 + x] = bc;
+			}
+		}
+	}
+	return;
+}
+
+void putblock8_8(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize)
+{
+	int x, y;
+	for (y = 0; y < pysize; y++) {
+		for (x = 0; x < pxsize; x++) {
+			vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+		}
 	}
 	return;
 }
---
+++ _notes/day-05.md
@@ -0,0 +1,11 @@
+## Use sprintf
+https://gitee.com/paud/30daysOS/tree/master/projects/05_day/harib02g
+
+这个sprintf是作者使用的c编译器自带的函数
+sprintf的特点是只操作内存，不依靠任何操作系统，因此可以在任何平台运行
+因此必须使用作者提供的Toolset(hrb.osask.jp)
+
+
+
+## Mouse 
+https://gitee.com/paud/30daysOS/tree/master/projects/05_day/harib02h
