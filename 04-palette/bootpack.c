d23459b012d56909095254ec3c155d03bdd7b919
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-01-03 21:19:05 -0800

    update

--- 04-palette/bootpack.c
+++ 04-palette/bootpack.c
@@ -40,8 +40,8 @@ void demo_fill_screen()
 	//   write_mem8(i, 15); // MOV BTYPE [i], 15
 	// }
 	char *p = (char *)0xa0000;
-
-	for (int i = 0; i <= 0xffff; i++)
+	int i;
+	for (i = 0; i <= 0xffff; i++)
 	{
 		*(p + i) = i & 0x0f;
 	}
