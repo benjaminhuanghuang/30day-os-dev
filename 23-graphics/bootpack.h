4d52fd6c74b5e9a1e32324b38c78e58689e2629f
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-01-14 19:25:31 -0800

    line

--- 23-graphics/bootpack.h
+++ 23-graphics/bootpack.h
@@ -266,6 +266,9 @@ void cmd_dir(struct CONSOLE *cons);
 void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline);
 int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
 int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax);
+int *inthandler0d(int *esp);
+int *inthandler0c(int *esp);
+void hrb_api_linewin(struct SHEET *sht, int x0, int y0, int x1, int y1, int col);
 
 /* file.c */
 struct FILEINFO {
--- 23-graphics/console.c
+++ 23-graphics/console.c
@@ -398,7 +398,7 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 	int ds_base = *((int *)0xfe8);
 	struct TASK *task = task_now();
 	struct CONSOLE *cons = (struct CONSOLE *)*((int *)0x0fec);
-	struct SHTCTL *shtctl = (struct SHTCTL *) *((int *) 0x0fe4);
+	struct SHTCTL *shtctl = (struct SHTCTL *)*((int *)0x0fe4);
 	struct SHEET *sht;
 	int *reg = &eax + 1; /* next to eax */
 	/* Forcibly rewrite PUSHAD for storage */
@@ -429,16 +429,62 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 		sheet_slide(sht, 100, 50);
 		sheet_updown(sht, 3);
 		reg[7] = (int)sht;
-	} else if (edx == 6) {
-		sht = (struct SHEET *) ebx;
-		putfonts8_asc(sht->buf, sht->bxsize, esi, edi, eax, (char *) ebp + ds_base);
-		sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
-	} else if (edx == 7) {
-		sht = (struct SHEET *) ebx;
-		boxfill8(sht->buf, sht->bxsize, ebp, eax, ecx, esi, edi);
-		sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
 	}
-
+	else if (edx == 6)
+	{
+		sht = (struct SHEET *) (ebx & 0xfffffffe);
+		putfonts8_asc(sht->buf, sht->bxsize, esi, edi, eax, (char *)ebp + ds_base);
+		if ((edx & 1) == 0)
+		{
+			sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
+		}
+	}
+	else if (edx == 7)
+	{
+		sht = (struct SHEET *) (ebx & 0xfffffffe);
+		boxfill8(sht->buf, sht->bxsize, ebp, eax, ecx, esi, edi);
+		if ((edx & 1) == 0)
+		{
+		sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+		}
+	}
+	else if (edx == 8)
+	{
+		memman_init((struct MEMMAN *)(ebx + ds_base));
+		ecx &= 0xfffffff0;
+		memman_free((struct MEMMAN *)(ebx + ds_base), eax, ecx);
+	}
+	else if (edx == 9)
+	{
+		ecx = (ecx + 0x0f) & 0xfffffff0;
+		reg[7] = memman_alloc((struct MEMMAN *)(ebx + ds_base), ecx);
+	}
+	else if (edx == 10)
+	{
+		ecx = (ecx + 0x0f) & 0xfffffff0;
+		memman_free((struct MEMMAN *)(ebx + ds_base), eax, ecx);
+	}
+	else if (edx == 11)
+	{
+		sht = (struct SHEET *) (ebx & 0xfffffffe);
+		sht->buf[sht->bxsize * edi + esi] = eax;
+		if ((edx & 1) == 0)
+		{
+		sheet_refresh(sht, esi, edi, esi + 1, edi + 1);
+		}
+	}
+	else if (edx == 12)
+	{
+		sht = (struct SHEET *)ebx;
+		sheet_refresh(sht, eax, ecx, esi, edi);
+	}
+	else if (edx == 13) {
+		sht = (struct SHEET *) (ebx & 0xfffffffe);
+		hrb_api_linewin(sht, eax, ecx, esi, edi, ebp);
+		if ((ebx & 1) == 0) {
+			sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+		}
+	}
 	return 0;
 }
 
@@ -463,4 +509,53 @@ int *inthandler0d(int *esp)
 	sprintf(s, "EIP = %08X\n", esp[11]);
 	cons_putstr0(cons, s);
 	return &(task->tss.esp0); /*强制结束程序*/
+}
+
+void hrb_api_linewin(struct SHEET *sht, int x0, int y0, int x1, int y1, int col)
+{
+	int i, x, y, len, dx, dy;
+
+	dx = x1 - x0;
+	dy = y1 - y0;
+	x = x0 << 10;
+	y = y0 << 10;
+	if (dx < 0) {
+		dx = - dx;
+	}
+	if (dy < 0) {
+		dy = - dy;
+	}
+	if (dx >= dy) {
+		len = dx + 1;
+		if (x0 > x1) {
+			dx = -1024;
+		} else {
+			dx =  1024;
+		}
+		if (y0 <= y1) {
+			dy = ((y1 - y0 + 1) << 10) / len;
+		} else {
+			dy = ((y1 - y0 - 1) << 10) / len;
+		}
+	} else {
+		len = dy + 1;
+		if (y0 > y1) {
+			dy = -1024;
+		} else {
+			dy =  1024;
+		}
+		if (x0 <= x1) {
+			dx = ((x1 - x0 + 1) << 10) / len;
+		} else {
+			dx = ((x1 - x0 - 1) << 10) / len;
+		}
+	}
+
+	for (i = 0; i < len; i++) {
+		sht->buf[(y >> 10) * sht->bxsize + (x >> 10)] = col;
+		x += dx;
+		y += dy;
+	}
+
+	return;
 }
