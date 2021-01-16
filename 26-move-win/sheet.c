b4484b3e454e042246c68262c9d7e74faf83039c
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-01-16 12:02:13 -0800

    speed up sheet.c

--- 26-move-win/sheet.c
+++ 26-move-win/sheet.c
@@ -157,67 +157,89 @@ void sheet_refreshmap(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, in
 
 void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0, int h1)
 {
-	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
+	int h, bx, by, vx, vy, bx0, by0, bx1, by1, bx2, sid4, i, i1, *p, *q, *r;
 	unsigned char *buf, *vram = ctl->vram, *map = ctl->map, sid;
 	struct SHEET *sht;
-	/* refresh範囲が画面外にはみ出していたら補正 */
-	if (vx0 < 0)
-	{
-		vx0 = 0;
-	}
-	if (vy0 < 0)
-	{
-		vy0 = 0;
-	}
-	if (vx1 > ctl->xsize)
-	{
-		vx1 = ctl->xsize;
-	}
-	if (vy1 > ctl->ysize)
-	{
-		vy1 = ctl->ysize;
-	}
-	for (h = h0; h <= h1; h++)
-	{
+	/* clip when the refresh range extends beyond the screen */
+	if (vx0 < 0) { vx0 = 0; }
+	if (vy0 < 0) { vy0 = 0; }
+	if (vx1 > ctl->xsize) { vx1 = ctl->xsize; }
+	if (vy1 > ctl->ysize) { vy1 = ctl->ysize; }
+
+	for (h = h0; h <= h1; h++) {
 		sht = ctl->sheets[h];
 		buf = sht->buf;
 		sid = sht - ctl->sheets0;
-		/* vx0〜vy1を使って、bx0〜by1を逆算する */
+		/* Use vx0 to vy1 to calculate bx0 to by1 */
 		bx0 = vx0 - sht->vx0;
 		by0 = vy0 - sht->vy0;
 		bx1 = vx1 - sht->vx0;
 		by1 = vy1 - sht->vy0;
-		if (bx0 < 0)
-		{
-			bx0 = 0;
-		}
-		if (by0 < 0)
-		{
-			by0 = 0;
-		}
-		if (bx1 > sht->bxsize)
-		{
-			bx1 = sht->bxsize;
-		}
-		if (by1 > sht->bysize)
-		{
-			by1 = sht->bysize;
-		}
-		for (by = by0; by < by1; by++)
-		{
-			vy = sht->vy0 + by;
-			for (bx = bx0; bx < bx1; bx++)
-			{
+		if (bx0 < 0) { bx0 = 0; }
+		if (by0 < 0) { by0 = 0; }
+		if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
+		if (by1 > sht->bysize) { by1 = sht->bysize; }
+		if ((sht->vx0 & 3) == 0) {
+			/* 4 byte */
+			i  = (bx0 + 3) / 4; /* bx0 divided by 4 (rounded up to the nearest whole number)  */
+			i1 =  bx1      / 4; /* bx1 divided by 4 (rounded down to the nearest whole number)  */
+			i1 = i1 - i;
+			sid4 = sid | sid << 8 | sid << 16 | sid << 24;
+			for (by = by0; by < by1; by++) {
+				vy = sht->vy0 + by;
+				for (bx = bx0; bx < bx1 && (bx & 3) != 0; bx++) {	/* 前面被4除多余的部分 */
+					vx = sht->vx0 + bx;
+					if (map[vy * ctl->xsize + vx] == sid) {
+						vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+					}
+				}
 				vx = sht->vx0 + bx;
-				if (map[vy * ctl->xsize + vx] == sid)
-				{
-					vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+				p = (int *) &map[vy * ctl->xsize + vx];
+				q = (int *) &vram[vy * ctl->xsize + vx];
+				r = (int *) &buf[by * sht->bxsize + bx];
+				for (i = 0; i < i1; i++) {							/* 4的倍数部分 */
+					if (p[i] == sid4) {
+						q[i] = r[i];
+					} else {
+						bx2 = bx + i * 4;
+						vx = sht->vx0 + bx2;
+						if (map[vy * ctl->xsize + vx + 0] == sid) {
+							vram[vy * ctl->xsize + vx + 0] = buf[by * sht->bxsize + bx2 + 0];
+						}
+						if (map[vy * ctl->xsize + vx + 1] == sid) {
+							vram[vy * ctl->xsize + vx + 1] = buf[by * sht->bxsize + bx2 + 1];
+						}
+						if (map[vy * ctl->xsize + vx + 2] == sid) {
+							vram[vy * ctl->xsize + vx + 2] = buf[by * sht->bxsize + bx2 + 2];
+						}
+						if (map[vy * ctl->xsize + vx + 3] == sid) {
+							vram[vy * ctl->xsize + vx + 3] = buf[by * sht->bxsize + bx2 + 3];
+						}
+					}
+				}
+				for (bx += i1 * 4; bx < bx1; bx++) {				/* 后面被4除多余的部分*/
+					vx = sht->vx0 + bx;
+					if (map[vy * ctl->xsize + vx] == sid) {
+						vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+					}
+				}
+			}
+		} else {
+			/* 1 byte */
+			for (by = by0; by < by1; by++) {
+				vy = sht->vy0 + by;
+				for (bx = bx0; bx < bx1; bx++) {
+					vx = sht->vx0 + bx;
+					if (map[vy * ctl->xsize + vx] == sid) {
+						vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+					}
 				}
 			}
 		}
 	}
 	return;
 }
+
 
 void sheet_updown(struct SHEET *sht, int height)
 {
