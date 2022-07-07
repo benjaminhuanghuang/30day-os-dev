11cd39c0d4c934745776b76e586fac7f4cd86e2d
benjamin huang <benjaminhuanghuang@gmail.com>
2022-07-06 22:27:19 -0700

    update day 3 c

--- 03-2-c/bootpack.c
+++ 03-2-c/bootpack.c
@@ -1,17 +1,19 @@
 
 extern void io_hlt(void);
 extern void write_mem8(int addr, int data);
+extern void myprint();
 
 
 void HariMain(void)
 {
-  int i;
+  // int i;
 
-	for(i = 0xa0000; i <= 0xaffff; i++){
-		write_mem8(i, i&0x0f);
-	}
-
+	// for(i = 0xa0000; i <= 0xaffff; i++){
+	// 	write_mem8(i, i&0x0f);
+	// }
+  myprint();
 	for(;;){
 		io_hlt();
 	}
+
 }
